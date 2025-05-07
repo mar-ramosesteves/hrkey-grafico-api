c5008132ea9e6ff08b6413d7b0a9b9f57cd54c5c
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-05-07 11:41:23 -0300

    Create ESCALAS.py

---
+++ ESCALAS.py
@@ -0,0 +1,39 @@
+ESCALAS = {
+    "Q1": {
+        "ESQUERDA": {
+            "Imperativo": 1,
+            "Resoluto": 2,
+            "Cuidativo": 3,
+            "Consultivo": 1,
+            "Prescritivo": 2,
+            "Formador": 3
+        },
+        "DIREITA": {
+            "Imperativo": 3,
+            "Resoluto": 2,
+            "Cuidativo": 1,
+            "Consultivo": 2,
+            "Prescritivo": 1,
+            "Formador": 1
+        }
+    },
+    "Q2": {
+        "ESQUERDA": {
+            "Imperativo": 2,
+            "Resoluto": 3,
+            "Cuidativo": 1,
+            "Consultivo": 2,
+            "Prescritivo": 1,
+            "Formador": 2
+        },
+        "DIREITA": {
+            "Imperativo": 1,
+            "Resoluto": 2,
+            "Cuidativo": 3,
+            "Consultivo": 1,
+            "Prescritivo": 3,
+            "Formador": 1
+        }
+    }
+    # ... até Q49
+}
